73414fc5980b7f90050488e091713612332f9129
Jeevan <gairejeevan7@gmail.com>
2024-07-04 07:54:18 +0530

    initial commit

---
+++ JavaScript fundamental 1/script.js
@@ -0,0 +1,94 @@
+/*
+let js = 'amazing';
+
+// if (js === 'amazing') {
+// alert('JavaScript is amazing!')
+// }
+
+console.log(48 + 8 + 23);
+
+let firstName = "Jeevan";
+
+console.log(firstName);
+
+// variable name convention
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'jonas';
+let PI = 3.1415; // capital name for the constant variable
+
+let myFirstJob = 'Coder'; // this type of variable name is best practice to use
+let myCurrentJob = 'QA';
+
+let job1 = 'Coder';
+let job2 = 'Teacher' ;
+
+console.log(myFirstJob);
+
+// DataTypes
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 123);
+console.log(typeof 'Jeevan');
+
+javaScriptIsFun = 'Fun';
+console.log(javaScriptIsFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+
+console.log(typeof null);
+
+const birthYear = 1991;
+// birthYear = 1990; --> we cannot muted const variable
+
+var job = 'Programmer';
+job = 'teacher';
+
+lastName = 'Sal'; // create variable in global
+console.log(lastName)
+*/
+
+// OPERATOR
+
+// Math Operators
+const now = 2081;
+const ageJonas = now - 1991;
+const ageSarah = now - 2011;
+console.log(ageJonas , ageSarah);
+
+console.log(ageJonas * 2, ageJonas /2, 2 ** 3);
+
+const firstName = 'Jeevan';
+const lastName = 'Gaire';
+console.log(firstName + lastName)
+console.log(firstName + " " + lastName)
+
+console.log(typeof firstName);
+
+// Assignment Operator
+let x = 10 + 4; // 14
+x += 10; // x = x + 10 = 24
+x *= 10; // 240
+x++; // x = x + 1 
+x--;
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah);
+console.log(ageJonas < ageSarah); // >, <, <=, >=, ===
+
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(isFullAge);
+
+console.log(now - 1991 > now - 2011);
