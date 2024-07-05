1693fb3d2199c52d5fffc6a59e73b0b56bb22438
Jeevan <gairejeevan7@gmail.com>
2024-07-05 13:03:52 +0530

    Equality operator

--- JavaScript fundamental 1/script.js
+++ JavaScript fundamental 1/script.js
@@ -171,8 +171,6 @@ console.log('23' + '10' + 3);
 let n = '1' + 1; // '1' + '1' = '11'
 n = n - 1;       // 11 - 1 = 10
 console.log(n);
-*/
-
 
 // Truthy and Falsy Value
 
@@ -198,4 +196,30 @@ if (height) {
     console.log("YAY! Height is defined");
 } else {
     console.log("Height is UNDEFINED");
+}
+    */
+
+const age = '18';
+if (age === 18) {
+    console.log("You just became an adult :D (strict)");
+}
+
+if (age == 18) {
+    console.log("You just became an adult :D (loose)");
+}
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23){
+    console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+    console.log("7 is also a cool number");
+} else {
+    console.log("Number is not 23 or 7");
+}
+
+if (favourite !== 23) {
+    console.log('Why not 23?');
 }
