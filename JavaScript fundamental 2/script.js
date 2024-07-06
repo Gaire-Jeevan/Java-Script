3e55f18405e94738b22633828d22d79f547c9808
Jeevan <gairejeevan7@gmail.com>
2024-07-06 07:20:23 +0530

    function basic

--- JavaScript fundamental 2/script.js
+++ JavaScript fundamental 2/script.js
@@ -2,6 +2,7 @@
 
 'use strict';
 
+/*
 let hasDriverLicence = false;
 const passTest = true;
 
@@ -15,3 +16,27 @@ if (hasDriverLicence) {
 
 // const interface = 'Audio'; // strict note reserve this word for future use
 // const private = 5434;
+*/
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+logger(); // invokin / calling / running function
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number('23'); // Build in function
