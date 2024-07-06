a991af4dc30817cc7f70e9b1fc23760e063c1a94
Jeevan <gairejeevan7@gmail.com>
2024-07-06 07:01:09 +0530

    strict note.

---
+++ JavaScript fundamental 2/script.js
@@ -0,0 +1,17 @@
+// Strict note
+
+'use strict';
+
+let hasDriverLicence = false;
+const passTest = true;
+
+if (passTest) {
+    hasDriverLicence = true;
+}
+
+if (hasDriverLicence) {
+    console.log("I can drive :D")
+}
+
+// const interface = 'Audio'; // strict note reserve this word for future use
+// const private = 5434;
