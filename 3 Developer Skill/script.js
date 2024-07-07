247763b4d3360d02ce1c1268b02b6d0b16517b81
Jeevan <gairejeevan7@gmail.com>
2024-07-07 09:57:44 +0530

    debug with the help of debugger and console

--- 3 Developer Skill/script.js
+++ 3 Developer Skill/script.js
@@ -1,6 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
 // PROBLEM:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
@@ -75,4 +76,61 @@ const calcTempAmplitudeNew = function (t1, t2) {
 };
 
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+*/
+
+const measureKelvin = function () {
+  const measeurement = {
+    type: 'temp',
+    unit: 'celsius',
+
+    // C) FIX --> making return of prompt to Number
+    // value: Number(prompt('Degree celsius: ')),
+    value: 10,
+  };
+
+  //   B) FIND
+  console.log(measeurement);
+  console.table(measeurement);
+
+  // console.log(measeurement.value);
+  // console.warn(measeurement.value);
+  // console.error(measeurement.value);
+
+  const kelvin = measeurement.value + 273;
+  return kelvin;
+};
+
+// A) IDENTIFY
+console.log(measureKelvin());
+
+// Fixing another bug -> Using debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    debugger;
+    if (curTemp > max) {
+      max = curTemp;
+    }
+
+    if (curTemp < min) {
+      min = curTemp;
+    }
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeNew = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+
+// A) Identify
 console.log(amplitudeNew);
