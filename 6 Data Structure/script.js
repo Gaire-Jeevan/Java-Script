a1da12b0bf8cc0fe72d4a585d84a84fed01f12e9
Jeevan <gairejeevan7@gmail.com>
2024-07-12 15:23:56 +0530

    Destructuring object

--- 6 Data Structure/script.js
+++ 6 Data Structure/script.js
@@ -8,11 +8,75 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  openingHour: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // open 24 hour
+      close: 24,
+    },
+  },
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({
+    time = '20:00', 
+    address, 
+    mainIndex = 0, 
+    starterIndex = 1}) 
+    {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  }
 };
 
+restaurant.orderDelivery({
+  time: '22:30',
+  address: "Here",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address : "Here",
+  starterIndex: 1,
+})
+
+// Object destructuring
+const { name, openingHour, categories } = restaurant;
+console.log(name, openingHour, categories);
+
+const {
+  name: restaurantName,
+  openingHour: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = [], starterMenu: starter = [] } = restaurant;
+console.log(menu, starter);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+// when curly braces come then javascript expect code block and in code block we cannot assign values so it is better to wrap inside paranthesis
+({a, b} = obj); 
+
+// nested objects
+const {fri : {open: o, close : c}} = openingHour;
+console.log(open, close);
+/*
+///////////////////////////////////////
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -52,3 +116,4 @@ console.log(i, j, k);
 // default values
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
+*/
