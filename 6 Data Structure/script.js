98eed66aa13084f5e02a518b6ee79fe90f95a02e
Jeevan <gairejeevan7@gmail.com>
2024-07-13 13:51:53 +0530

    introduction to map

--- 6 Data Structure/script.js
+++ 6 Data Structure/script.js
@@ -69,6 +69,42 @@ const restaurant = {
 };
 
 ////////////////////////////////////////////////////////////////////////
+// Maps
+
+const rest = new Map();
+rest.set('name', 'The Royal family');
+rest.set(1, 'The Grand Plaza');
+console.log(rest.set(2, 'Kathmandu, Nepal'));
+
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']).set('open',11)
+.set('close',23)
+.set(true, 'We are open :D')
+.set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+
+// rest.clear();
+// rest.set([1, 2], 'Test')
+const arr = [1, 2]
+rest.set(arr, 'Test')
+
+rest.set(document.querySelector('h1'), 'heading');
+
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+
+/*
+////////////////////////////////////////////////////////////////////////
 // SETS
 
 const orderSet = new Set([
@@ -105,7 +141,8 @@ console.log(staffUnique);
 console.log(new Set(['Waiter', 'Chief', 'Waiter', 'Manager', 'Chief', 'Waiter']).size);
 
 console.log(new Set('jeevanGaire').size);
-/*
+
+
 /////////////////////////////////////////////////////////
 // Looping Object
 
