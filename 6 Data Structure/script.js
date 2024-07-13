cdb29f259cd74320465a7f5522b58f56b7a4c5ef
Jeevan <gairejeevan7@gmail.com>
2024-07-13 07:32:54 +0530

    Local assignment operator

--- 6 Data Structure/script.js
+++ 6 Data Structure/script.js
@@ -50,6 +50,44 @@ const restaurant = {
   },
 };
 
+// const rest1 = {
+//   name: 'Capri',
+//   numGuests: 20,
+// };
+
+const rest1 = {
+  name: 'Capri',
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'The Royal Place',
+  owner: 'Ramesh',
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// local assignment operator
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator 
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// OR assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+/*
+///////////////////////////////////////////////////////////
 // The Nullish Coalescing Operator
 restaurant.numGuests = 0;
 const guests1 = restaurant.newGuests ? restaurant.numGuests : 10;
@@ -62,7 +100,7 @@ console.log(guests2);
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
 
-/*
+
 //////////////////////////////////////////////////////////
 
 // Short circuiting (&& and ||)
