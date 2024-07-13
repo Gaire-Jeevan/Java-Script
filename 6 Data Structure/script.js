ba110c48460702551db335f2550d23ea288fcedf
Jeevan <gairejeevan7@gmail.com>
2024-07-13 14:46:00 +0530

    map iteration

--- 6 Data Structure/script.js
+++ 6 Data Structure/script.js
@@ -68,8 +68,48 @@ const restaurant = {
   },
 };
 
+/////////////////////////////////////////////////////////////////////////
+// Map Iteration
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct🎉'],
+  [false, 'Try again!'],
+]);
+
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHour));
+const hoursMap = new Map(Object.entries(openingHour));
+
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if(typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+const answer = Number(prompt("Your answer"));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log(...question);
+console.log(question.entries());
+console.log(...question.keys());
+console.log(...question.values());
+/*
 ////////////////////////////////////////////////////////////////////////
-// Maps
+// Maps: Fundamental
 
 const rest = new Map();
 rest.set('name', 'The Royal family');
@@ -103,7 +143,7 @@ console.log(rest.size);
 console.log(rest.get(arr));
 
 
-/*
+
 ////////////////////////////////////////////////////////////////////////
 // SETS
 
