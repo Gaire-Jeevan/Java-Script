ee1ccef263063fa0107a04b911535639c03bc3a6
Jeevan <gairejeevan7@gmail.com>
2024-07-14 14:21:37 +0530

    Higher order function (function accepting function 'call back function'

--- 7 Function/script.js
+++ 7 Function/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 /*
+////////////////////////////////////////////////////
+// Default Parameter 
+
 const bookings = [];
 
 const createBooking = function(
@@ -28,8 +31,10 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
-*/
 
+
+//////////////////////////////////////////////////
+// Passing argument workflow 
 const flight = 'LH234';
 const jonas = {
     name: 'Jonas Sen',
@@ -62,3 +67,35 @@ const newPassport = function(person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+////////////////////////////////////////////////////
+// Higher Order Function (function accepting function)
+
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+ 
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+// Higher-order function
+const transformer = function(str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed String: ${fn(str)}`);
+    
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callback all the time
+const high5 = function() {
+    console.log('👋');
+}
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martin', 'Adam'].forEach(high5);
