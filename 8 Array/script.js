357e56b9eadd4786028f8c4d36d0e73297d62d0b
Jeevan <gairejeevan7@gmail.com>
2024-07-15 07:37:34 +0530

    new array method

--- 8 Array/script.js
+++ 8 Array/script.js
@@ -73,6 +73,7 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+/*
 /////////////////////////////////////////////////
 
 let arr = ['a', 'b', 'c', 'd', 'e'];
@@ -105,3 +106,21 @@ console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join(' - '));
+*/
+
+///////////////////////////////////////////////////
+// New Array methods
+
+// at
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice[-1][0]);
+console.log(arr.at(-1));
+
+// for string
+console.log('john'.at(0));
