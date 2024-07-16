0d2e41a3d88220d0580ef218751f04b9e32a2a4f
Jeevan <gairejeevan7@gmail.com>
2024-07-16 21:50:24 +0530

    implementing requesting loan (some / every method)

--- 8 Array/script.js
+++ 8 Array/script.js
@@ -192,6 +192,22 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value)
+
+  if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUi(currentAccount);
+  }
+  inputLoanAmount.value = '';
+})
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -456,3 +472,28 @@ console.log(accounts);
 const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 console.log(account);
 */
+
+
+//////////////////////////////////////////////////////////////////////
+
+console.log(movements);
+
+// SOME Method
+
+// EQUALITY
+console.log(movements.includes(-130));
+
+// CONDITION
+console.log(movements.some(mov => mov === 130));
+
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+// EVERY
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
