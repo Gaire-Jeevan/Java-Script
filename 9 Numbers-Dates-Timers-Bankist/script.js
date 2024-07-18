428851bb2ba29b74c3c65273fda05e95c5d26eee
Jeevan <gairejeevan7@gmail.com>
2024-07-18 22:43:43 +0530

    remainder operator

--- 9 Numbers-Dates-Timers-Bankist/script.js
+++ 9 Numbers-Dates-Timers-Bankist/script.js
@@ -291,7 +291,7 @@ console.log(Number.isFinite(23 / 0));
 console.log(Number.isInteger(23));
 console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
-*/
+
 
 console.log(Math.sqrt(25));
 console.log((25 ** (1/2)));
@@ -330,3 +330,31 @@ console.log(Math.trunc('-23.9'));
 console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(5));
 console.log(+(2.7).toFixed(5));
+*/
+
+// Remainder
+
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 % 3);
+console.log(8 % 3);
+
+console.log(6 % 2);
+console.log(6 % 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(2));
+console.log(isEven(3));
+console.log(isEven(0));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) {
+      row.style.backgroundColor = 'orangered';
+    }
+    if (i % 3 === 0) {
+      row.style.backgroundColor = 'blue';
+    }
+  });
+});
