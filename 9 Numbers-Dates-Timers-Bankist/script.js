41568001422f81c8dc929fea43392da4fc6583ce
Jeevan <gairejeevan7@gmail.com>
2024-07-20 16:51:29 +0530

    implementing a countdown timer

--- 9 Numbers-Dates-Timers-Bankist/script.js
+++ 9 Numbers-Dates-Timers-Bankist/script.js
@@ -196,9 +196,37 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc);
 };
 
+const startLogOutTimer = function() {
+  const tick = function() {
+    const min = String(Math.trunc(time / 60)).padStart(2,0);
+    const sec = String(time % 60).padStart(2,0);
+
+    // In each call, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+  
+    // When 0 seconds, stop timer and log out user
+    if(time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = `Log in to get started`;
+      containerApp.style.opacity = 0;
+    }
+    
+    // Decrease 1s
+    time--;
+  };
+  // Set time to 5 minutes
+  let time = 600;
+
+  // Call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
+
+}
 ///////////////////////////////////////
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
 
 // FAKE ALWAYS LOGGED IN
 
@@ -272,6 +300,12 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
 
+    // startLogOutTimer();
+    if(timer) {
+      clearInterval(timer);
+    }
+    timer = startLogOutTimer();
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -301,6 +335,10 @@ btnTransfer.addEventListener('click', function (e) {
 
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
   }
 });
 
@@ -319,6 +357,10 @@ btnLoan.addEventListener('click', function (e) {
 
     // Update UI
     updateUI(currentAccount);
+
+    // Reset timer
+    clearInterval(timer);
+    timer = startLogOutTimer();
     }, 2500);
     
   }
@@ -579,7 +621,7 @@ console.log('US: ', new Intl.NumberFormat('en-US', options).format(num));
 console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
 console.log('Syria: ', new Intl.NumberFormat('ar-SY', options).format(num));
 console.log(navigator.language, new Intl.NumberFormat(navigator.language, options).format(num));
-*/
+
 
 /////////////////////////////////////////////////////////////////
 // Timers_setTmeout and setInterval
@@ -617,3 +659,4 @@ const day = `${date.getDate()}`.padStart(2, 0);
 // const month = `${date.getMonth() + 1}`.padStart(2, 0);
 // const year = date.getFullYear();
 // return `${day}/${month}/${year}`;
+*/
