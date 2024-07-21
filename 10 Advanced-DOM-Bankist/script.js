87a838c98454ac2b32230bc53b5d2be3cf4f2bbd
Jeevan <gairejeevan7@gmail.com>
2024-07-21 16:38:59 +0530

    style, attribute and classes

--- 10 Advanced-DOM-Bankist/script.js
+++ 10 Advanced-DOM-Bankist/script.js
@@ -33,6 +33,7 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
 ////////////////////////////////////////////////////
 
 // Selecting Elements
@@ -45,7 +46,7 @@ const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
 
-document.getElementById('seciton--1');
+document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button')
 console.log(allButtons);
 
@@ -56,7 +57,7 @@ console.log(document.getElementsByClassName('btn'));
 // .insertAdjacentHTML
 
 const message = document.createElement('div');
-message.classList.add('cookie-messaage');
+message.classList.add('cookie-message');
 // message.textContent = 'We use cookied for improved functionality and analytics.';
 message.innerHTML = 'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
@@ -69,9 +70,57 @@ header.append(message); // last child
 
 // Delete elements
 document.querySelector(".btn--close-cookie").addEventListener('click', function() {
-  message.remove();
+  // message.remove();
   message.parentElement.removeChild(message);
 });
 
-header.insertAdjacentHTML(afterbegin,
+header.insertAdjacentHTML('afterbegin',
   '<div>Welcome my boy</div>');
+
+// Styles
+message.style.backgroundColor = '#37373d';
+message.style.width = '120%';
+
+// only display the properties that is manually set
+console.log(message.style.backgroundColor);
+// doesn't display it as it is not manually set
+console.log(message.style.color);
+
+// display property even if it is not present
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = parseFloat(getComputedStyle(message).height, 10) + 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+console.log(logo.src);
+
+logo.alt = "Beautiful minmalist logo"
+// Non-standard
+console.log(logo.designer);
+console.log(logo.getAttribute('designer'));
+console.log(logo.setAttribute('company', 'Bankist'));
+
+console.log(logo.src);
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.twitter-link');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data Attribute
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+// Don't use
+logo.className = 'jonas'
