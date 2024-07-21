034bc3f01135ee16edc82cc37e7b12697dd67bb3
Jeevan <gairejeevan7@gmail.com>
2024-07-21 19:41:51 +0530

    event propagation (capturing, target and bubbling phase)

--- 10 Advanced-DOM-Bankist/script.js
+++ 10 Advanced-DOM-Bankist/script.js
@@ -163,7 +163,7 @@ btnScrollTo.addEventListener('click', function (e) {
   // modern way
   section1.scrollIntoView({behavior: "smooth"});
 });
-*/
+
 
 //////////////////////////////////////////////////
 
@@ -186,3 +186,37 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000)
 // h1.onmouseenter = function(e) {
 //   alert('onmouseenter: Great! You are reading the heading :D')
 // }
+*/
+
+//////////////////////////////////////////////////
+// Event Propagation (Capturing , Bubbling and Capturing)
+
+// rgb(255, 255, 255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+  
+  // STOP PROPAGATION
+
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+}, true);
