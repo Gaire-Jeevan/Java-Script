bd6fb9790089c464d35f48ff0ac9b031e478537d
Jeevan <gairejeevan7@gmail.com>
2024-07-22 18:36:24 +0530

    passing arguments to event handlers

--- 10 Advanced-DOM-Bankist/script.js
+++ 10 Advanced-DOM-Bankist/script.js
@@ -7,6 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 ///////////////////////////////////////
 // Modal window
@@ -102,33 +106,57 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 ///////////////////////////////////////////////////////////////////////
 // Tabbed component
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 // tabs.forEach(t=> t.addEventListener('click', () => console.log('tab')));
 
 // using event delegation
-tabsContainer.addEventListener('click', function(e) {
+tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
   // Guard clause
-  if(!clicked) return;
+  if (!clicked) return;
 
-   // Removing active classes from tab and content area
+  // Removing active classes from tab and content area
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
   tabsContent.forEach(c => c.classList.remove('operations__content--active'));
-
 
   // Active tab
   clicked.classList.add('operations__tab--active');
 
-
   // Activate content area
 
-  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
-})
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
 
+// Menu fade animation
+const handleHover = function (e, opactity) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      // if (el !== link) el.style.opacity = opactity;
+      if (el !== link) el.style.opacity = this;
+    });
+    // logo.style.opacity = opactity;
+    logo.style.opacity = this;
+  }
+};
+
+// Passing "argument" into handler
+// nav.addEventListener('mouseover', function (e) {
+//   handleHover(e, 0.5);
+// });
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+// undo the upper function
+// nav.addEventListener('mouseout', function (e) {
+//   handleHover(e, 1);
+// });
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 /*
 ////////////////////////////////////////////////////
