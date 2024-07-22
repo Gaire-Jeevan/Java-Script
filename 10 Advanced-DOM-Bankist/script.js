202ea01050bee8efa9a9f7405af76bf0c371a84c
Jeevan <gairejeevan7@gmail.com>
2024-07-22 18:04:50 +0530

    building a tabbed component

--- 10 Advanced-DOM-Bankist/script.js
+++ 10 Advanced-DOM-Bankist/script.js
@@ -100,6 +100,36 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+///////////////////////////////////////////////////////////////////////
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// tabs.forEach(t=> t.addEventListener('click', () => console.log('tab')));
+
+// using event delegation
+tabsContainer.addEventListener('click', function(e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if(!clicked) return;
+
+   // Removing active classes from tab and content area
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+
+  // Activate content area
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+})
+
+
 /*
 ////////////////////////////////////////////////////
 
@@ -287,7 +317,7 @@ document.querySelector('.nav').addEventListener('click', function (e) {
   console.log('NAV', e.target, e.currentTarget);
   console.log(e.currentTarget === this);
 }, true);
-*/
+
 
 ////////////////////////////////////////////////////
 
@@ -321,4 +351,5 @@ console.log(h1.parentElement.children);
   if (el !== h1) {
     el.style.transform = 'scale(0.5)'
   }
-})
+});
+*/
