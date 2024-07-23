19ca381e83f336647c03dc027865e9ff43835db2
Jeevan <gairejeevan7@gmail.com>
2024-07-23 19:02:27 +0530

    constructor function

---
+++ 11 OOP/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Person = function (firstName, birthYear) {
+    console.log(this);
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never do this
+    this.calcAge = function() {
+        console.log(2037 - this.birthYear);
+    }
+};
+
+const jeevan = new Person('Jeevan', 2003);
+console.log(jeevan);
+// 1. New {} (object)is created
+// 2. function is called, this = {}
+// 3. {} to prototype
+// 4. function automatically return {}
+
+const matlida = new Person('matlida', 2017);
+const jack = new Person('jack', 1975);
+console.log(matlida, jack);
+
+console.log(jeevan instanceof Person);
+
