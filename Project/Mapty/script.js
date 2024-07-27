5fea44cfa91e54aa80fdf8999569616e716dafba
Jeevan <gairejeevan7@gmail.com>
2024-07-27 22:55:25 +0530

    move to marker on click

--- Project/Mapty/script.js
+++ Project/Mapty/script.js
@@ -3,6 +3,7 @@
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
+  click = 0;
 
   constructor(coords, distance, duration) {
     // this.date = ...
@@ -19,6 +20,10 @@ class Workout {
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`;
+  }
+
+  clicks() {
+    this.click++
   }
 }
 
@@ -69,10 +74,13 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+
 class App {
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workout = [];
+
 
   constructor() {
     this._getPosition();
@@ -80,6 +88,8 @@ class App {
     form.addEventListener('submit', this._newWorkout.bind(this));
 
     inputType.addEventListener('change', this._toggleElevationField);
+   
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this))
   }
 
   _getPosition() {
@@ -100,7 +110,7 @@ class App {
 
     const coords = [latitude, longitude];
 
-    this.#map = L.map('map').setView(coords, 13);
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
@@ -269,6 +279,29 @@ class App {
     }
 
     form.insertAdjacentHTML('afterend', html);
+  }
+  
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+    console.log(workoutEl);
+
+    if(!workoutEl) {
+      return;
+    }
+
+    const workout = this.#workout.find(work => work.id === workoutEl.dataset.id);
+    console.log(workout);
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      }
+    })
+
+    // using the public interface
+    workout.click()
   }
 }
 
