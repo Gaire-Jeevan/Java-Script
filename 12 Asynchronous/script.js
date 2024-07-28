f129f3df9416401ac7529dbcfbe7ed211278c2d2
Jeevan <gairejeevan7@gmail.com>
2024-07-28 14:10:05 +0530

    chaining promises

--- 12 Asynchronous/script.js
+++ 12 Asynchronous/script.js
@@ -129,9 +129,14 @@ setTimeout(() => {
 //   request.send();
 
 const renderCountry = function (data, className = '') {
+//   console.log(data);
+// if array is passed then only 1st element is taken
+  if (Array.isArray(data)) {
+    data = data[0];
+  }
   const html = `
             <article class="country ${className}">
-              <img class="country__img" src="${data.flags.png}" />
+              <img class="country__img" src="${data.flags.svg}" />
               <div class="country__data">
                 <h3 class="country__name">${data.name.common}</h3>
                 <h4 class="country__region">${data.region}</h4>
@@ -151,29 +156,39 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-const request = fetch('https://restcountries.com/v3.1/name/nepal');
-console.log(request);
-
-const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(function (response) {
-      console.log(response);
-      // to read body
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      renderCountry(data[0]);
-    });
-};
-
 // const request = fetch('https://restcountries.com/v3.1/name/nepal');
 // console.log(request);
 
 // const getCountryData = function (country) {
 //   fetch(`https://restcountries.com/v3.1/name/${country}`)
-//     .then((response) => response.json())
-//     .then( (data) => renderCountry(data[0]));
+//     .then(function (response) {
+//       console.log(response);
+//       // to read body
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
 // };
-getCountryData('portugal');
-getCountryData('usa');
+
+// const request = fetch('https://restcountries.com/v3.1/name/nepal');
+// console.log(request);
+
+const getCountryData = function (country) {
+  // Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      //   Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+getCountryData('nepal');
