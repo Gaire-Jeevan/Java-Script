f031bf19944ea0f2f8d396c05a86d649e9884de5
Jeevan <gairejeevan7@gmail.com>
2024-07-28 21:08:14 +0530

    event loop in practice

--- 12 Asynchronous/script.js
+++ 12 Asynchronous/script.js
@@ -151,7 +151,7 @@ setTimeout(() => {
     }, 1000)
     
 }, 1000)
-*/
+
 
 /////////////////////////////////////////////////////
 
@@ -268,3 +268,19 @@ btn.addEventListener('click', function () {
 });
 
 getCountryData('australia');
+
+*/
+
+/////////////////////////////////////////////////
+
+console.log('Test Start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('resolved promise 1').then(res => console.log(res)
+
+)
+Promise.resolve('resolved promise 2').then(res => {
+  for(let i = 0; i<1000; i++){
+    console.log(res);
+  }
+})
+console.log('test end');
