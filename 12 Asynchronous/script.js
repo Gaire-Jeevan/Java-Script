444b3dce96ca04c704fe237665218898ed88f785
Jeevan <gairejeevan7@gmail.com>
2024-07-30 19:41:07 +0530

    running promises in parallel

--- 12 Asynchronous/script.js
+++ 12 Asynchronous/script.js
@@ -36,6 +36,14 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(`${errorMsg} (${response.status})`);
+    }
+    return response.json();
+  });
+};
 ///////////////////////////////////////
 /*
 const getCountryData = function (country) {
@@ -396,7 +404,7 @@ const whereAmI = function () {
 };
 
 btn.addEventListener('click', whereAmI);
-*/
+
 
 ////////////////////////////////////////////////
 // Consuming Promises
@@ -472,3 +480,30 @@ console.log('1. Will get location');
 // } catch(err) {
 //   alert(err.message)
 // }
+*/
+
+//////////////////////////////////////////////////////////////////
+// Running Promises in Parallel
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    
+    // console.log(data1.capital, data2.capital, data3.capital);
+
+    const data = Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+
+    console.log((await data).map(d => d[0].capital));
+
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('nepal', 'portugal', 'india');
